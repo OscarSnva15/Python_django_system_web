25e79c4b54715f577591bec42817584bf5534fd6
Oscar Suarez Nava <oscarsnva15@gmail.com>
2025-01-06 16:02:23 -0600

    create a models django

--- my_app/models.py
+++ my_app/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 # Create your models here.
 # los modelos sireven para crear tablas provienentes de una base de datos 
-class Project(models.Model):
-    name = models.CharField(max_length=200)
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    public = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
     
-class Task(models.Model):
-    tittle = models.CharField(max_length=200)
-    description = models.TextField()
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=250)
+    created_at = models.DateTimeField()
+    
