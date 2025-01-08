065a459a95c4c4e8e6ddb257848bbf1cad48ccc9
Oscar Suarez Nava <oscarsnva15@gmail.com>
2025-01-08 16:47:13 -0600

    add filters database

--- my_app/urls.py
+++ my_app/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('contacs/', views.contacs, name="contacs"),
     path('create_article/<str:title>/<str:content>/<str:public>', views.create_article, name="create_article"),
     path('view-article/', views.view_article, name="view_article"),
-    #path('contactos-dos/<str:nombre>', views.contactos, name="contacto"),
+    path('edit_article/<int:id>', views.edit_article, name="edit_article"),
+    path('show_article/', views.show_article, name="show_article"), 
+    path('delete_article/<int:id>', views.delete_article, name="delete_article"),
     #path('contactos-dos/<str:nombre>/<str:apellidos>', views.contactos, name="contacto"),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from my_app.models import Article,Category
+from django.db.models import Q
 
 # Create your views here. send files html
 def index(request):
@@ -59,4 +60,37 @@ def view_article(request):
         article = Article.objects.get(title="Superman", public=True)
         return HttpResponse(f"Articulo: {article.id}{article.title}{article.title}")
     except:
-        return HttpResponse("Articulo no encontrado")    
+        return HttpResponse("Articulo no encontrado")
+
+def edit_article(request,id):
+    article = Article.objects.get(pk=id)
+    article.title = "Robin"
+    article.content = "Robin es amigo de Batman"
+    article.public = True
+    article.save()
+    return HttpResponse(f"Articulo editado: {article.title} - {article.content}")
+
+def show_article(request):
+    #article = Article.objects.all()
+    #article = Article.objects.order_by('id')
+    #article = Article.objects.order_by('title')
+    #article = Article.objects.order_by('-id')
+    #article = Article.objects.order_by('-title') #aplica el ordenamiento de manera descendente
+    #article = Article.objects.order_by('-id')[:1] #plica limit a mi consulta
+    #article = Article.objects.filter(title = 'Superman', id=8) #aplica un filtro a mi consulta
+    #article = Article.objects.filter(title__exact='superman') #aplica un filtro a mi consulta
+    #article = Article.objects.filter(id__lt=12) #aplica un filtro de id, menores a 12
+    #article = Article.objects.filter(id__lte=10) #aplica un filtro de id, menores a 10 o igual a 10
+    #article = Article.objects.filter(id__lte=10, title__contains="2") #aplica un filtro de id, mayores a 10 y que contenga el numero 2 en el titulo
+    #article = Article.objects.filter(title = 'Article').exclude(public = False) #metodo que exluya el public a true
+    
+    #article = Article.objects.raw("SELECT * FROM my_app_article WHERE public=1") #consulta sql
+    
+    article = Article.objects.filter(
+        Q(title__contains="2") | Q(title__contains="3") ) #aplica un filtro de id, menores a 12
+    return render(request,'articles.html',{'articles':article})
+
+def delete_article(request,id):
+    article = Article.objects.get(pk=id)
+    article.delete()
+    return redirect('show_article')#redireccionar a la vista show_article
